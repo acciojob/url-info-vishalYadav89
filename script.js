9e073506dbf5695871455072657b7ce853538afe
Vishal Yadav <ydvishal89@gmail.com>
2026-03-17 21:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL
+var currentURL = window.location.href;
+
+// Get the length of the URL
+var urlLength = currentURL.length;
+
+// Show the result in an alert
+alert("The length of the URL is: " + urlLength);
